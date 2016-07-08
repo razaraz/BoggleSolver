802e395f0037a2033d3137a95be857970c6361a5
Ramon Zarazua B <killerfox512@gmail.com>
2016-07-08 15:16:00 -0700

    Initial commit

---
+++ BoggleManaged/Boggle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace BoggleManaged
+{
+    public class Boggle
+    {
+        public Boggle(uint width, uint height, Char[] board)
+        {
+            Width = width;
+            Height = height;
+            Board = board;
+        }
+
+        public uint Width { get; private set; }
+        public uint Height { get; private set; }
+        public Char[] Board { get; private set; }
+
+        public IEnumerable<String> Solve()
+        {
+        }
+
+        private class DictNode
+        {
+            Char letter;
+            DictNode[] children;
+        }
+
+        private async Task<DictNode> ProcessDictionaryNodesAsync();
+    }
+}
