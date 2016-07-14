5670ef3af72d70e339ec8a24618406464464d19a
Ramon Zarazua B <killerfox512@gmail.com>
2016-07-14 01:13:02 -0700

    Final tweaks, displaying reports.

--- Start-BoggleBuild.ps1
+++ Start-BoggleBuild.ps1
@@ -1,6 +1,15 @@
+###############################################################################
+# File: Start-BoggleBuild.ps1
+# Author: Ramón Zarazúa B.
+# Date: Jul-14-2016
+###############################################################################
+
 
 $ErrorActionPreference = "Stop"
 
+###############################################################################
+#                             Verification                                    #
+###############################################################################
 Write-Host -ForegroundColor Blue "Verifying prerequisites"
 $MSBuildVerRegKey = 'HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0'
 
@@ -46,14 +55,16 @@ else
 
 Write-Host -ForegroundColor Blue "Prerequisites verified"
 
-# Compile
+###############################################################################
+#                                Compiling                                    #
+###############################################################################
 Write-Host -ForegroundColor Blue "Compiling project"
 $SolutionName = "Boggle.sln"
 $SolutionPath = Join-Path $PSScriptRoot $SolutionName
 $Configuration = "Release"
 $Platform = "x64"
 
-Write-Host -ForegroundColor DarkBlue "`tInvoking MSBuild for:`n`t`tSolution: $SolutionName`n`t`tConfiguration: $Configuration`n`t`tPlatform: $Platform"
+Write-Host -BackgroundColor DarkBlue "`tInvoking MSBuild for:`n`t`tSolution: $SolutionName`n`t`tConfiguration: $Configuration`n`t`tPlatform: $Platform"
 $MSBuildArgs = [string[]]@(
                             $SolutionPath,
                             "/property:Configuration=$Configuration",
@@ -77,35 +88,52 @@ if($MSBuildProc.ExitCode -ne 0)
 
 Write-Host -ForegroundColor Blue "Compilation Successful."
 
-# Run Unit Tests
+###############################################################################
+#                                   Test                                      #
+###############################################################################
 Write-Host -ForegroundColor Blue "Running Unit Tests."
 
-#  Interface Tests
 $UnitTestAssembly = "BoggleTests\bin\Release\BoggleTests.dll"
-$MSTestArgs = [string[]]@(
-                            "/testcontainer:$UnitTestAssembly",
-                            "/category:Interface"
-)
-$MSTestProc = Start-Process -FilePath $MSTestExePath -ArgumentList $MSTestArgs -PassThru
 
-$MSTestProc.WaitForExit();
-echo "MSTest Exit code $($MSTestProc.ExitCode)"
+function Run-MSTest($TestContainer, $Category)
+{ 
+	$MSTestArgs = [string[]]@(
+								"/testcontainer:$UnitTestAssembly"
+	)
 
-# Performance Tests
-$MSTestPerfArgs = [string[]]@(
-                            "/testcontainer:$UnitTestAssembly",
-                            "/category:Performance",
-                            "/detail:duration"
-)
-$MSTestProc = Start-Process -FilePath $MSTestExePath -ArgumentList $MSTestPerfArgs -PassThru
+	if($Category)
+	{ 
+		$MSTestArgs += "/category:Interface"
+	}
 
-$MSTestProc.WaitForExit();
-echo "MSTest Exit code $($MSTestProc.ExitCode)"
+	Write-Host -BackgroundColor DarkBlue "`tInvoking MSTest for:`n`t`tTestContainer: $TestContainer`n`t`tCategory: Category"
+	$MSTestProc = Start-Process -FilePath $MSTestExePath -ArgumentList $MSTestArgs -PassThru -WindowStyle Hidden
 
-# Parse MSTest Report
+	$MSTestProc.WaitForExit();
 
-# Generate Report Summary
+	$ResultsFile = Get-ChildItem "$PSScriptRoot\TestResults\" -File | Sort-Object CreationTime | Select-Object -Last 1 -ExpandProperty FullName
+	if(!(Test-Path $ResultsFile))
+	{
+		Write-Error "Could not find the results trx file"
+	}
+	
+	# Parse trx, and display summary
+	$Results = [xml](Get-Content $ResultsFile)
 
+	Write-Host -ForegroundColor DarkGreen "Test run completed. Results:"
+	$Results.TestRun.ResultSummary.Counters | select executed, passed, error, failed, timeout | Format-Table | Out-String | Write-Host -ForegroundColor DarkGreen
+	"Duration: {0}" -f ([datetime]$Results.TestRun.Times.finish - [datetime]$Results.TestRun.Times.start).ToString() | Write-Host -ForegroundColor DarkGreen
+
+	if($MSTestProc.ExitCode)
+	{
+		start $ResultsFile
+		Write-Error "Tests Failed. Opening up the results file in Visual Studio."
+	}
+}
+
+Run-MSTest -TestContainer $UnitTestAssembly -Category "Interface"
+Run-MSTest -TestContainer "BoggleTests\bin\Release\FunctionalTests.orderedtest"
+Run-MSTest -TestContainer $UnitTestAssembly -Category "Performance"
 
 Write-Host -ForegroundColor Green "Build and testing success. Press any key to continue."
 
